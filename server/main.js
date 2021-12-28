fee64e567b60f44bc7bc62f6b849868f6c7347f2
aakash-shakya <official.aakas@gmail.com>
2021-12-28 05:59:48 -0800

    mongoose db connection & application server setup

--- server/main.js
+++ server/main.js
@@ -10,3 +10,12 @@ app.use(bodyParser.json({ limit : "30mb", extended : true }))
 app.use(bodyParser.urlencoded({ limit : "30mb", extended = true }))
 
 app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://root:root@cluster0.wlelx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running successfully on ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
